082da52950a2533c42fb8029ba4be82accddbd73
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-22 17:44:51 +0100

    feat number Generator Pc

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-// Mail
+
+
+
+// Chiedi all’utente la sua email... 
+
+// const mail = prompt ("inserisci la tua mail per ottenere udienza presso il circolo dei re invisibili");
+
+// console.log(mail);
+
+// // controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. 
+
+// // // Creo una lista con una const array 
+
+// // const emailList = [ "genoveffa@gmail.com" , "asdrubale@gmail.com" , "sosipatro@gmail.com" , "clodomilda@gmail.com"]; 
+
+// // // Controllo che la mail inserita dall'utente sia inserita nell'elenco 
+
+// // if (emailList.includes(mail)){
+// //     alert("Benvenuto! Ti stavamo aspettando!") }
+
+// //     else {  alert ("Chi sei? Accesso negato!")
+// }
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
@@ -13,22 +34,9 @@
 // proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
 
 
-// Chiedi all’utente la sua email... 
+// Number Generator per PC 
 
-const mail = prompt ("inserisci la tua mail per ottenere udienza presso il circolo dei re invisibili");
+const dadiPc = Math.floor (Math.random ()*6) +1;
 
-console.log(mail);
+console.log(dadiPc);
 
-// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. 
-
-// Creo una lista con una const array 
-
-const emailList = [ "genoveffa@gmail.com" , "asdrubale@gmail.com" , "sosipatro@gmail.com" , "clodomilda@gmail.com"]; 
-
-// Controllo che la mail inserita dall'utente sia inserita nell'elenco 
-
-if (emailList.includes(mail)){
-    alert("Benvenuto! Ti stavamo aspettando!") }
-
-    else {  alert ("Chi sei? Accesso negato!")
-}
